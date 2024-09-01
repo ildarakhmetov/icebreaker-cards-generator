d0c56cb30f3fe49cf50c26f93147a08352642d32
Ildar Akhmetov <ildar.akhmetov@gmail.com>
2024-09-01 16:59:27 -0700

    refactor: Add Rush language and Red Hat distribution to config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Edit this file to change the configuration
 
-LANGUAGES = ["Python", "Java", "C", "C++", "JavaScript", "Ruby", "Go"]
-DISTRIBUTIONS = ["Ubuntu", "Fedora", "Debian", "Arch", "CentOS", "Mint", "SUSE"]
+LANGUAGES = ["Python", "Java", "C", "C++", "JavaScript", "Ruby", "Go", "Rush"]
+DISTRIBUTIONS = ["Ubuntu", "Fedora", "Debian", "Arch", "CentOS", "Mint", "SUSE", "Red Hat"]
 PIONEERS = ["Alan Turing", "Grace Hopper", "Donald Knuth", "Ada Lovelace",
-            "Edsger Dijkstra", "Tim Berners-Lee", "Linus Torvalds"]
+            "Edsger Dijkstra", "Tim Berners-Lee", "Linus Torvalds", "Bill Gates"]
 NUM_STUDENTS = 36
