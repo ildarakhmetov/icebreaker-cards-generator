bd4d5f6a77d9ed5775714650dfaeb14b3ca3de37
Ildar Akhmetov <ildar.akhmetov@gmail.com>
2024-09-01 17:00:28 -0700

    typo

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Edit this file to change the configuration
 
-LANGUAGES = ["Python", "Java", "C", "C++", "JavaScript", "Ruby", "Go", "Rush"]
+LANGUAGES = ["Python", "Java", "C", "C++", "JavaScript", "Ruby", "Go", "Rust"]
 DISTRIBUTIONS = ["Ubuntu", "Fedora", "Debian", "Arch", "CentOS", "Mint", "SUSE", "Red Hat"]
 PIONEERS = ["Alan Turing", "Grace Hopper", "Donald Knuth", "Ada Lovelace",
             "Edsger Dijkstra", "Tim Berners-Lee", "Linus Torvalds", "Bill Gates"]
